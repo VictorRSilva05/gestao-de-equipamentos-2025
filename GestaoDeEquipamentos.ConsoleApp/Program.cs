a9392578a259c6f1442164bc9a9febb1d36b93c9
Victor R. Silva <victorr.silva005@gmail.com>
2025-05-08 08:48:38 -0300

    Implementado visualização de fabricantes

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using GestaoDeEquipamentos.ConsoleApp.Compartilhado;
+using GestaoDeEquipamentos.ConsoleApp.ModuloFabricante;
 using GestaoDeEquipamentos.ConsoleApp.Util;
+using System.Text;
 
 namespace GestaoDeEquipamentos.ConsoleApp;
 
@@ -13,12 +15,37 @@ class Program
 
         app.MapGet("/", PaginaInicial);
 
+        app.MapGet("/fabricantes/visualizar", VisualizarFabricantes);
+
         app.Run();
+    }
+
+    static Task VisualizarFabricantes(HttpContext context)
+    {
+        ContextoDados contextoDados = new ContextoDados(true);
+        IRepositorioFabricante repositorioFabricante = new RepositorioFabricanteEmArquivo(contextoDados);
+
+        string conteudo = File.ReadAllText("ModuloFabricante/Html/Visualizar.html");
+
+        StringBuilder stringBuilder = new StringBuilder(conteudo);
+        foreach (Fabricante fabricante in repositorioFabricante.SelecionarRegistros())
+        {
+            string itemLista = $"<li>{fabricante.ToString()}</li> #fabricante#";
+
+            stringBuilder.Replace("#fabricante#", itemLista);
+
+        }
+
+        stringBuilder.Replace("#fabricante#", "");
+
+        string conteudoString = stringBuilder.ToString();
+
+        return context.Response.WriteAsync(conteudo);
     }
 
     static Task PaginaInicial(HttpContext context)
     {
         string conteudo = File.ReadAllText("Html/PaginaInicial.html");
         return context.Response.WriteAsync(conteudo);
-    }   
+    }
 }
